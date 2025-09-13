6f9a8945971d352c998a9070966045b7de76ae31
PipoGonzalez59 <alexjosemilio@gmail.com>
2025-09-12 21:33:38 -0600

    Add files via upload

---
+++ Curso.java
@@ -0,0 +1,37 @@
+package com.studentorganizer.model;
+
+import java.awt.Color;
+
+public class Curso {
+	private String nombre;
+    private String descripcion;
+    private Color color;
+    
+    public Curso(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.color = Color.BLUE; // Color por defecto
+    }
+    
+    public Curso(String nombre, String descripcion, Color color) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.color = color;
+    }
+    
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    
+    public Color getColor() { return color; }
+    public void setColor(Color color) { this.color = color; }
+    
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}

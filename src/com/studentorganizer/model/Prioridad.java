b1343ba36db149e24d72f24fb58db79a61e0176c
PipoGonzalez59 <alexjosemilio@gmail.com>
2025-10-17 11:29:12 -0600

    Update Prioridad.java
    Removed unnecessary blank line at the end of the file.

--- src/com/studentorganizer/model/Prioridad.java
+++ src/com/studentorganizer/model/Prioridad.java
@@ -2,7 +2,9 @@ package com.studentorganizer.model;
 
 public enum Prioridad {
 	ALTA("Alta"),
+	
     MEDIA("Media"),
+	
     BAJA("Baja");
     
     private final String displayName;
@@ -15,5 +17,4 @@ public enum Prioridad {
     public String toString() {
         return displayName;
     }
-
 }

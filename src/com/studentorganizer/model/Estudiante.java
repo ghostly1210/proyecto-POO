5378f86893dfd343878712531dd15253ae188050
PipoGonzalez59 <alexjosemilio@gmail.com>
2025-10-17 11:28:31 -0600

    Update Estudiante.java

--- src/com/studentorganizer/model/Estudiante.java
+++ src/com/studentorganizer/model/Estudiante.java
@@ -20,7 +20,7 @@ public class Estudiante {
         this.agenda = new Agenda();
     }
     
-    // Métodos de negocio
+    // Métodos
     public void agregarTarea(Tarea nuevaTarea) {
         listaDeTareas.add(nuevaTarea);
         // Agregar a la agenda también
@@ -51,23 +51,46 @@ public class Estudiante {
         return tareasCompletadas;
     }
     
-    // Getters y Setters
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getNombre() { 
+		return nombre; 
+	}
+    public void setNombre(String nombre) { 
+		this.nombre = nombre; 
+	}
     
-    public String getCarrera() { return carrera; }
-    public void setCarrera(String carrera) { this.carrera = carrera; }
+    public String getCarrera() {
+		return carrera; 
+	}
+    public void setCarrera(String carrera) { 
+		this.carrera = carrera; 
+	}
     
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+		return email;
+	}
+    public void setEmail(String email) {
+		this.email = email; 
+	}
     
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() { 
+		return password;
+	}
+    public void setPassword(String password) { 
+		this.password = password; 
+	}
     
-    public List<Tarea> getListaDeTareas() { return listaDeTareas; }
-    public void setListaDeTareas(List<Tarea> listaDeTareas) { this.listaDeTareas = listaDeTareas; }
+    public List<Tarea> getListaDeTareas() {
+		return listaDeTareas; 
+	}
+    public void setListaDeTareas(List<Tarea> listaDeTareas) { 
+		this.listaDeTareas = listaDeTareas;
+	}
     
-    public Agenda getAgenda() { return agenda; }
-    public void setAgenda(Agenda agenda) { this.agenda = agenda; }
+    public Agenda getAgenda() { 
+		return agenda;
+	}
+    public void setAgenda(Agenda agenda) { 
+		this.agenda = agenda; 
+	}
 
 }
